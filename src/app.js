7507dbb8652a4c279c3c80c24b83edfd97024b89
[Sandeep] <sandeep977244@gmail.com>
2026-06-28 18:08:16 +0530

    Implement note creation and retrieval endpoints

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 // Server ko create krna 
 const express = require('express')
 
-const app = express();
+const notes = [];
 
+const app = express();
+app.use(express.json());
+
+app.post('/notes', (req, res) => {
+    notes.push(req.body)
+    
+    res.status(201).json({
+        message: "Note created successfully "
+        
+    })
+})
+
+app.get('/notes', (req, res)=>{
+    res.status(200).json({
+        message: "Note fetch successfully !",
+        notes: notes
+    })
+})
 module.exports = app;
