0827f39b637beac2fb3e3ab9490406cfe28183b4
[Sandeep] <sandeep977244@gmail.com>
2026-07-01 22:47:21 +0530

    Get started with database !

--- db.js
+++ db.js
@@ -2,3 +2,12 @@
 const mongoose = require('mongoose');
 
 // Connect to the MongoDB database.
+
+mongoose.connect('mongodb://localhost:27017/notes', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
