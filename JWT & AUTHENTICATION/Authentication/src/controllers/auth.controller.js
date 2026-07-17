75f8c6c5ea358b620c61e74c846a3857acaa2c44
[Sandeep] <sandeep977244@gmail.com>
2026-07-17 11:39:57 +0530

    Get started with Authentication

---
+++ JWT & AUTHENTICATION/Authentication/src/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+const userModel = require('../model/user.model');
+const jwt = require('jsonwebtoken');
+
+async function register(req, res) {
+    const {username,email,password} = req.body;
+
+
+    const user = await userModel.create({username,email,password});        
+
+
+    const token = jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET)
+
+
+    res.status(201).json({
+        message:"User registered successfully",
+        user,
+        token
+    })
+}
+
+
+
+
+
+
+
+module.exports = { register }
